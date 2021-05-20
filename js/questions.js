71ebcb7e032edb440aee3f0c246989a7e90830ac
Sanket Pawar <pawarsanket668@gmail.com>
2021-05-20 12:35:36 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,264 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is marketing?",
+    answer: "All of above",
+    options: [
+      "Marketing is not about promotion or advertising only",
+      "Marketing is complex topic or a concept",
+      "This is used to make a comprehensive plan of action to generate leads, increase sales, increase brand awareness or acquire more customers",
+      "All of above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Effective marketing requires?",
+    answer: "All of above",
+    options: [
+      "Careful planning",
+      "Creative approaches",
+      "Smart execution",
+      "All of above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "It’s used by marketer to attract people who perform searches that imply they’re interested in learning about a particular industry?",
+    answer: "Search engine optimization",
+    options: [
+      "Search engine optimization",
+      "Print marketing",
+      "Social media marketing",
+      "Video marketing"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Digital marketing also known as?",
+    answer: "Both",
+    options: [
+      "Internet Marketing",
+      "Online Marketing",
+      "Both",
+      "None"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which marketing use the online platform to promote the product and services of our company.",
+    answer: "Online marketing",
+    options: [
+      "Traditional marketing",
+      "Online marketing",
+      "Both",
+      "None"
+    ]
+  },
+    {
+    numb: 6,
+    question: "SEO is most important part of which marketing?",
+    answer: "Digital marketing",
+    options: [
+      "Digital marketing",
+      "Online marketing",
+      "Traditional Marketing",
+      "None"
+    ]
+  },
+    {
+    numb: 7,
+    question: "SMS marketing can very effective for real estate, ----------- And ----------------.",
+    answer: "Education and healthcare business",
+    options: [
+      "Colleges and university.",
+      "Education and society",
+      "Collage and student",
+      "Education and healthcare business"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Content marketing is a digital marketing strategy where marketing is done using-----------.",
+    answer: "Both",
+    options: [
+      "High –quality",
+      "Relevant content",
+      "Both",
+      "None"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Marketing automation is future of ---------?",
+    answer: "Email marketing",
+    options: [
+     " Social media marketing ",
+      "Email marketing", 
+      "SMS marketing ",
+      "Search engine marketing ",
+
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the 5 Ds Digital marketing?",
+    answer: "All three",
+    options: [
+      "Device, platform",
+      "Data, media", 
+      "Data, technology",
+      "All three", 
+
+    ]
+  },
+    {
+    numb: 11,
+    question: "------------------ is the second largest search engine after Google.",
+    answer: "YouTube",
+    options: [
+      "Facebook",
+      "Instagram",
+      "YouTube",
+      "Twitter",
+ 
+
+    ]
+  },
+    {
+    numb: 12,
+    question: "----------------- is when the marketing activities are undertaken in a conventional fashion",
+    answer: "Traditional marketing",
+    options: [
+      "Digital marketing",
+      "Traditional marketing",
+      "Both",
+      "None",
+      
+
+    ]
+  },
+    {
+    numb: 13,
+    question: "----------------------- is method of paying to post sponsored.",
+    answer: "Instagram",
+    options: [
+     "Instagram",
+      "Facebook",
+      "YouTube",
+      "Twitter",
+      
+
+    ]
+  },
+    {
+    numb: 14,
+    question: "E-mail marketing is a very effective medium for------------------.",
+    answer: "Communication",
+    options: [
+      "Automation", 
+      "Communication", 
+      "Automation & communication",
+     " None",
+      
+
+    ]
+  },
+    {
+    numb: 15,
+    question: "Instagram is part of the ----------- network only",
+    answer: "Facebook",
+    options: [
+      "Twitter", 
+      "Facebook",
+      "YouTube", 
+      "LinkIn",
+
+
+    ]
+  },
+    {
+    numb: 16,
+    question: "In ------------, 120 million instagrammers visited a website , got direction , called , emailed, or direct messaged to learn about a business based on an Instagram ad",
+    answer: "March 2017",
+    options: [
+      "March 2018",
+      "March 2015",
+      "March 2017",
+      "March 2016",
+
+
+
+    ]
+  },
+    {
+    numb: 17,
+    question: "-------------- Creating and publishing all kinds of videos that entertain and educate their core customer",
+    answer: "Video marketing",
+    options: [
+      "Search engine marketing", 
+      "Print marketing",
+      "Video marketing", 
+      "Blog marketing",  
+
+
+
+    ]
+  },
+    {
+    numb: 18,
+    question: "----------- are no longer exclusive to the individual writer.",
+    answer: "Blog",
+    options: [
+      "Blog",
+      "Print",
+      "Video",
+      "Internet",
+
+
+
+    ]
+  },
+    {
+    numb: 19,
+    question: "SEO is part of inbound marketing and ------------------------.",
+    answer: "Search engine marketing",
+    options: [
+      "Print marketing" ,
+     "Search engine marketing",
+      "Email marketing" ,
+      "SMS marketing" ,
+      
+
+
+    ]
+  },
+    {
+    numb: 20,
+    question: "According to Instagram, ---------- of people say they discover new products on the platform.",
+    answer: "60%",
+    options: [
+      "80%",
+      "45%",
+     "60%",
+      "100%",
+
+
+
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
